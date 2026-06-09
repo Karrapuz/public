eac46917a9b485f6ada48be384f14f5cb2f43be4
Karrapuz <nikolaiaminow@gmail.com>
2026-06-09 20:46:24 +0300

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,1 +1,19 @@
-
+export default {
+  title: "Моя Вики",
+  description: "База знаний",
+  base: '/', // Оставляем косую черту для своего домена
+  themeConfig: {
+    nav: [
+      { text: 'Главная', link: '/' },
+      { text: 'Статьи', link: '/about' }
+    ],
+    sidebar: [
+      {
+        text: 'Начало работы',
+        items: [
+          { text: 'О компании', link: '/about' }
+        ]
+      }
+    ]
+  }
+}
